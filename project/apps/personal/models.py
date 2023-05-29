ca8fa505b9e7f506dc253bbab42a7aa210159c67
Nahuel Cerdan <nahuel.cerdan.12@gmail.com>
2023-05-29 20:28:47 -0300

    Se registraron modelos Personal,Sector,Pais

--- project/apps/personal/models.py
+++ project/apps/personal/models.py
@@ -8,10 +8,10 @@ class Pais(models.Model):
         return self.nombre_pais
 
 class Sector (models.Model):
-    nombre_sector = models.CharField(max_length=50, unique=True)
+    nombre_sector = models.CharField(max_length=50)
     
     def __str__(self) -> str:
-            return self.nombre_sector,
+            return self.nombre_sector
 
 
 class Personal (models.Model):
@@ -19,7 +19,7 @@ class Personal (models.Model):
     apellido = models.CharField(max_length=50)  
     nacimiento = models.DateField(null=True)    
     pais_origen = models.ForeignKey(Pais,on_delete=models.SET_NULL, null=True)
-    sector =  models.ForeignKey(Sector,on_delete=models.SET_NULL, null=True)
+    sector = models.ForeignKey(Sector,on_delete=models.SET_NULL, null=True)
     def __str__(self) -> str:
         return self.nombre
     
