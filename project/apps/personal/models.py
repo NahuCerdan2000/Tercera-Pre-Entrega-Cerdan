2c2b02ed2162cb31272b779164aaba06e7547d75
Nahuel Cerdan <nahuel.cerdan.12@gmail.com>
2023-05-29 20:07:48 -0300

    Se agregaron Migraciones.

--- project/apps/personal/models.py
+++ project/apps/personal/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Pais(models.Model):
-    nombre = models.CharField(max_length=50, unique=True)
+    nombre_pais = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
-        return self.nombre
+        return self.nombre_pais
 
 class Sector (models.Model):
-    nombre = models.CharField(max_length=50, unique=True)
+    nombre_sector = models.CharField(max_length=50, unique=True)
     
     def __str__(self) -> str:
-            return self.nombre,
+            return self.nombre_sector,
 
 
 class Personal (models.Model):
